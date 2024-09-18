373b5113ee7e4245d000d2743847183418036297
erichard56 <enrique.richard@gmail.com>
2024-09-18 09:29:05 -0300

    saco crispy

--- hunglin/hunglin/settings.py
+++ hunglin/hunglin/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'crispy_forms',
-    'crispy_bootstrap5',
     'bosquetaoista',
 ]
 
@@ -150,6 +148,4 @@ MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'mediaprod/')
 STATIC_URL = '/mediaprod/'
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
